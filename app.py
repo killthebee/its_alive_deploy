828ac686402ebe90b12e9a46210280d13a98f1c2
Danny <superhotfire447@yandex.ru>
2020-11-26 12:55:29 +0300

    test 12

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ def is_alive_host(hostname):
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
     try:
         response = requests.get(hostname)
-        host_status = 'up' if response.status_code >= 100 and response.status_code < 400 else 'down'
+        host_status = 'up test' if response.status_code >= 100 and response.status_code < 400 else 'down test'
         return {'status': host_status}
     except ConnectionError:
-        return {'status': 'down'}
+        return {'status': 'down test'}
     except MissingSchema:
         return is_alive_host(f'http://{hostname}')
 
